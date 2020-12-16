0606474c93853d035d9bb1b115c27c4cd2483551
fredie <contact@funabia.com>
2020-12-16 17:47:06 +0000

    Add individual local and production environment files

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const sessionsRoute = require("./routes/sessions");
 
-const app = express();
+require("dotenv").config({ path: `./.env.${process.env.NODE_ENV}` });
 
-dotenv.config();
+const app = express();
 
 mongoose.connect(process.env.DB_CONNECT, { useUnifiedTopology: true }, () =>
   console.log("Connected to database...\n")
@@ -18,5 +17,5 @@ app.use("/api/sessions", sessionsRoute);
 
 app.get("/", (req, res) => res.send("boop"));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}...\n`));
