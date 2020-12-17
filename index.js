40d3b2ea78cd4bad47b3e2e7c02e04899be082b9
fredie <contact@funabia.com>
2020-12-17 23:20:56 +0000

    Fix call to cors middleware

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ mongoose.connect(process.env.DB_CONNECT, { useUnifiedTopology: true }, () =>
   console.log("Connected to database...\n")
 );
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use("/api/users", usersRoute);
 app.use("/api/sessions", sessionsRoute);
