de236412f1f5a99ba1dd9afe27946f29c42c8897
fredie <contact@funabia.com>
2020-12-18 17:58:22 +0000

    Add the UI build dir to serve from the root path of the domain

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 const usersRoute = require("./routes/users");
 const sessionsRoute = require("./routes/sessions");
 
@@ -14,10 +15,9 @@ mongoose.connect(process.env.DB_CONNECT, { useUnifiedTopology: true }, () =>
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client/build")));
 app.use("/api/users", usersRoute);
 app.use("/api/sessions", sessionsRoute);
-
-app.get("/", (req, res) => res.send("boop"));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}...\n`));
